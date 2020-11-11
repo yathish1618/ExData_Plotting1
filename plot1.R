18f74baf3df08ba43fbdace209c045a3c39f405a
Yathish <yathish.iitkgp@gmail.com>
2020-11-11 13:48:33 +0530

    Submitting assignment

---
+++ plot1.R
@@ -0,0 +1,18 @@
+#Download and read the raw data set
+if(!file.exists("exdata_data_household_power_consumption.zip")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "exdata_data_household_power_consumption.zip")
+}
+df <- read.table(unz("exdata_data_household_power_consumption.zip","household_power_consumption.txt"), header = TRUE, sep = ";", stringsAsFactors = FALSE)
+
+#Append an extra column to store Date/Time class object
+df[,"DateTime"] <- with(df, as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"))
+
+#Subset data containing data between 2007-02-01 and 2007-02-02
+strt <- as.POSIXct("2007-02-01", format = "%Y-%m-%d")
+end <- as.POSIXct("2007-02-03", format = "%Y-%m-%d")
+sub_df <- subset(df, DateTime >= strt & DateTime < end)
+
+#Open a png device, plot the graph and close the device
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg=NA)
+hist(as.numeric(sub_df$Global_active_power), col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,21 @@
+#Download and read the raw data set
+if(!file.exists("exdata_data_household_power_consumption.zip")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "exdata_data_household_power_consumption.zip")
+}
+df <- read.table(unz("exdata_data_household_power_consumption.zip","household_power_consumption.txt"), header = TRUE, sep = ";", stringsAsFactors = FALSE)
+
+#Append an extra column to store Date/Time class object
+df[,"DateTime"] <- with(df, as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"))
+
+#Subset data containing data between 2007-02-01 and 2007-02-02
+strt <- as.POSIXct("2007-02-01", format = "%Y-%m-%d")
+end <- as.POSIXct("2007-02-03", format = "%Y-%m-%d")
+sub_df <- subset(df, DateTime >= strt & DateTime < end)
+
+#Open a png device, plot the graph and close the device
+png(filename = "plot3.png", width = 480, height = 480, units = "px", bg=NA)
+with(sub_df,plot(DateTime, as.numeric(Sub_metering_1), type = "l", xlab = "",ylab = "Energy sub metering", main = ""))
+with(sub_df,points(DateTime, as.numeric(Sub_metering_2), col = "red", type = "l"))
+with(sub_df,points(DateTime, as.numeric(Sub_metering_3), col = "blue", type = "l"))
+legend("topright",lwd = 1, col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
